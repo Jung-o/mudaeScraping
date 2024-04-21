847e02f7c8b831449cc2dd2033068be35aadde07
Hugo RAPS <hugo.raps02@gmail.com>
2024-04-21 23:02:24 +0200

    [+] get character gender and series name

--- main.py
+++ main.py
@@ -32,8 +32,30 @@ def get_all_characters():
     return characters
 
 
+def get_character(character_url):
+    response = requests.get(character_url)
+    if response.status_code != 200:
+        print('Failed to get the page. Status code:', response.status_code)
+        time.sleep(5)
+        get_character(character_url)
+        return None
+    soup = BeautifulSoup(response.text, 'html.parser')
+    info = {}
+    series_div = soup.find(attrs={'data-source': 'series'})
+    series_name = series_div.find('a').text
+    info['series'] = series_name
+
+    gender_div = soup.find(attrs={'data-source': 'gender'})
+    gender = gender_div.find('div').text
+    info['gender'] = gender
+
+    return info
+
+
 def main():
     all_characters = get_all_characters()
+    for character in all_characters:
+        get_character(character)
 
 
 if __name__ == "__main__":
